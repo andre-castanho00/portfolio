0738b5dfa0f9bfea1fd6568de23cc8ace3a2115e
andre-castanho00 <201902108@estudantes.ips.pt>
2025-06-22 21:32:56 +0100

    light and night mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,12 @@ import Header from "./components/header/header";
 import Hero from "./components/hero/hero";
 import Projects from "./components/projects/projects";
 import Skills from "./components/skills/skills";
-import { useEffect, useState } from "react";
 
 /**
  * The main app component, which simply renders all the other components.
  * @returns {React.ReactElement} The JSX for the app.
  */
 function App() {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme) {
-      setTheme(storedTheme);
-    } else {
-      localStorage.setItem("theme", "light");
-    }
-
-    document.documentElement.setAttribute("data-theme", theme);
-  }, [theme]);
-
   return (
     <>
       <Header />
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,4 +1,6 @@
 import "./hero.css";
+import "./themeToggle.css";
+import { useState, useEffect } from "react";
 
 /**
  * @function Hero
@@ -7,9 +9,31 @@ import "./hero.css";
  * @returns {React.ReactElement} A JSX element representing the hero section.
  */
 function Hero() {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
   return (
     <section id="home" className="hero">
       <div className="container">
+        <label className="switch">
+          <input type="checkbox" onChange={toggleTheme} checked={isDarkMode} />
+          <span className="slider"></span>
+        </label>
         <h1>André Castanho</h1>
         <p>
           Full Stack Developer | Sotfware Engineer | Building Digital Solutions
